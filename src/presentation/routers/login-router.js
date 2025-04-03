b4bf0ceec1b894a21e4c7939de5d88b4ce5b0228
Jorge de Souza <jorgedesouzajacintto_jr@hotmail.com>
2025-04-03 19:55:47 -0300

    feat: transform route and auth to async

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,11 +1,11 @@
 const HttpResponse = require('../helpers/http-response')
 
 module.exports = class LoginRouter {
-  constructor (authUseCase) {
+  constructor(authUseCase) {
     this.authUseCase = authUseCase
   }
 
-  route (httpRequest) {
+  async route(httpRequest) {
     try {
       const { email, password } = httpRequest.body
       if (!email) {
@@ -14,7 +14,7 @@ module.exports = class LoginRouter {
       if (!password) {
         return HttpResponse.badRequest('password')
       }
-      const accessToken = this.authUseCase.auth(email, password)
+      const accessToken = await this.authUseCase.auth(email, password)
       if (!accessToken) {
         return HttpResponse.unauthorizedError()
       }
